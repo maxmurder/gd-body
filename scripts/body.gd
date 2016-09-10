b2b338a1f2f06c34e50065c434ff700d658d21dc
Clayton Campbell <ccampbell@mstar.io>
2016-09-09 17:29:12 -0700

    Work on body initilization and structure

--- scripts/body.gd
+++ scripts/body.gd
@@ -1,18 +1,61 @@
 
 extends Node
 
+export(String, FILE) var materialsFile
+export(String, FILE) var systemFile
+
+onready var _materialdata = loadjson(materialsFile)
+onready var _systemdata = loadjson(systemFile)
 var _limblist = []
-var _materials
+var _systems = {}
 
 func _ready():
-    processbody(self)
-    _materials = loadjson("res://materials.json")
-    for l in _limblist:
-        print(l.size)
-    print(_materials["MUSCLE"].values())
+    _limblist = processbody(self)
+    _systems = processsystems(_limblist, _systemdata)
+    print(checkstatus(_systems["VITAL"], 1.0))
     pass
 
-func loadjson(path):
+func _process(delta):
+    print(checkstatus(_systems["VITAL"], 1.0))
+    pass
+
+func checkstatus(system, threshold, type = "INTEGRITY", material = "NONE"):
+    for i in system:
+        var node = get_node(i)
+        if material == "NONE":
+            for l in node.limb_layers:
+                if node.limb_layers[l][type] < threshold:
+                    return false
+        else:
+            if node.limb_layers.has(material):
+                if node.limb_layers[material][type] < threshold:
+                    return false
+                else:
+                    return checkstatus(system, threshold)
+    return true
+
+static func processbody(node):
+    var list = []
+    for n in node.get_children():
+        if n.get_child_count() > 0:
+            for l in processbody(n):
+                list.append(l)
+        list.append(n)
+    return list
+
+static func processsystems(limblist, systemlist):
+    var dict = {}
+    for s in systemlist:
+        var list = []
+        for l in limblist:
+            for i in l.limb_flags:
+                if s == i:
+                    list.append(l.get_path())
+        if list.size() > 0:
+            dict[s] = list
+    return dict
+
+static func loadjson(path):
     var dict = {}
     var file = File.new()
     if !file.file_exists(path):
@@ -21,10 +64,3 @@ func loadjson(path):
     dict.parse_json(file.get_as_text())
     file.close()
     return dict
-
-func processbody(node):
-    for n in node.get_children():
-        if n.get_child_count() > 0:
-            processbody(n)
-        _limblist.append(n)
-    pass
--- scripts/limb.gd
+++ scripts/limb.gd
@@ -1,6 +1,12 @@
 extends Node
 
 export var size = 1.0
-export var internal = false
-export var vital = false
-export var material = ['NONE']
+export var material_layers = StringArray()
+export var limb_flags = StringArray()
+onready var limb_layers = processlayers(material_layers)
+
+static func processlayers(materials):
+    var dict = {}
+    for i in range(materials.size()):
+        dict[materials[i]] = { LAYER = i, INTEGRITY = 1.0, DAMAGE = 0.0}
+    return dict
