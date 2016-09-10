685bff8628899bfe2ee7f20f277fdecfac34a848
maxmurder <rusty657@shaw.ca>
2016-09-10 02:13:05 -0700

    more stuff

--- scripts/limb.gd
+++ scripts/limb.gd
@@ -4,6 +4,18 @@ export var size = 1.0
 export var material_layers = StringArray()
 export var flags = StringArray()
 onready var layers = processlayers(material_layers)
+var attached = true
+
+func detach():
+    attached = false
+    for n in self.get_children():
+        n.detach()
+
+func damage(damage):
+    for l in layers:
+        l["INTEGRITY"] = l["INTEGRITY"] - damage
+        l["DAMAGE"] = l["DAMAGE"] + damage
+    pass
 
 static func processlayers(materials):
     var dict = {}
--- scripts/testbody.gd
+++ scripts/testbody.gd
@@ -1,20 +1,49 @@
 extends "res://scripts/body.gd"
 
+export(float) var systemThreshold = 0.5
+export(float) var vitalThresholdIntegrity = 1.0
+export(float) var vitalThresholdDamage = 0.0
+
 func _ready():
-    print(checkstatus(_systems["SENSORY"], 1.0, "NERVOUS"))
+    set_process(true)
+    set_process_unhandled_input(true)
     pass
 
-func checkstatus(system, threshold, material = "NONE", type = "INTEGRITY"):
-    for i in system:
-        var node = get_node(i)
-        if material == "NONE":
+func _process(delta):
+    for i in _systems.keys():
+        var s = "%s : %s"
+        print(s % [i,checkstatus(_systems[i])])
+    pass
+
+func _unhandled_input(ev):
+    if ev.type == InputEvent.MOUSE_BUTTON:
+        if ev.is_pressed():
+            var i = randi() % _limbs.keys().size()
+            print(_limbs.keys()[i])
+            severlimb(_limbs.keys()[i])
+    pass
+
+func severlimb(limb):
+    var node = get_node(limb)
+    var parent = node.get_parent()
+    node.detach()
+    #parent.damage()
+    pass
+
+func checkstatus(system):
+    for n in system:
+        var node = get_node(n)
+        if "VITAL" in node.flags:
             for l in node.layers:
-                if node.layers[l][type] < threshold:
+                if node.layers[l]["INTEGRITY"] < vitalThresholdIntegrity or node.layers[l]["DAMAGE"] > vitalThresholdDamage:
+                    return false
+                if !node.attached:
                     return false
         else:
-            if node.layers.has(material):
-                if node.layers[material][type] < threshold:
-                    return false
-                else:
-                    return checkstatus(system, threshold)
+            for l in node.layers:
+                if node.layers[l]["INTEGRITY"] > systemThreshold:
+                    return true
+                if node.attached:
+                    return true
+            return false
     return true
